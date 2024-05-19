0109d98aa87e0e44c792104360df6091928c4feb
Patinya Jaruariyanon <patinya.jaru@gmail.com>
2024-05-20 05:02:02 +0700

    Add shake sign button when login fails

--- The Swift Arcade Professional iOS Development Course/Bankey/Bankey/Controller/Login/LoginViewController.swift
+++ The Swift Arcade Professional iOS Development Course/Bankey/Bankey/Controller/Login/LoginViewController.swift
@@ -147,6 +147,7 @@ extension LoginViewController: LoginViewDelegate {
     func configureView(withMessage message: String) {
         errorMessageLabel.text = message
         errorMessageLabel.isHidden = false
+        shakeButton()
     }
 }
 
@@ -165,5 +166,16 @@ extension LoginViewController {
             self.view.layoutIfNeeded()
         }
         animator2.startAnimation(afterDelay: 0.25)
+    }
+    
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
