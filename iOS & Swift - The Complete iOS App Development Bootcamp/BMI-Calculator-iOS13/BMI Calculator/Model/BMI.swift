c22eec9722be88290f9e7d438b847b3c8aa11119
Patinya Jaruariyanon <149204731+patinya2001@users.noreply.github.com>
2024-06-03 05:32:01 +0700

    Add BMI-Calculator-iOS13 to iOS & Swift - The Complete iOS App Development Bootcamp course

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/BMI-Calculator-iOS13/BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by ปฏิญญา จารุอริยานนท์ on 24/4/2567 BE.
+//  Copyright © 2567 BE Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
